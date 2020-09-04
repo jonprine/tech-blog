7e15b2e69ceab4e80b0e623999c1f94cc07d1f4b
Vox Humana <jonprine@gmail.com>
2020-09-04 13:25:43 -0500

    updated put route to use bcrypt

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,8 +51,8 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
