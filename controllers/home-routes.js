36b857276bc9a7ccbbd8fe9c4560ecdd3c23e557
Vox Humana <jonprine@gmail.com>
2020-09-04 17:53:42 -0500

    data template to homepage handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,34 @@ const router = require('express').Router();
 
 const sequelize = require('../config/connection');
 
-const { User } = require('../models');
+const { Post, User } = require('../models');
 
 router.get('/', (req, res) => {
-        res.render('homepage');
-    });
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('homepage', { posts });
+
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
