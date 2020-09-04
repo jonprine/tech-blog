eb9e76027bc6156d777f0423483b3f16d2136f8b
Vox Humana <jonprine@gmail.com>
2020-09-04 16:59:44 -0500

    changed to homepage. changed content inside main handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,10 +5,7 @@ const sequelize = require('../config/connection');
 const { User } = require('../models');
 
 router.get('/', (req, res) => {
-    User.findAll().then((users) => {
-        console.log(users[0]);
-        res.render("home", {users: users});
-    })
-})
+        res.render('homepage');
+    });
 
 module.exports = router;
