2580d9fa815009eae2e179b41457899f67332352
Vox Humana <jonprine@gmail.com>
2020-09-05 19:42:38 -0500

    added comments to handlebars homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const sequelize = require('../config/connection');
 
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
   console.log(req.session);
@@ -14,6 +14,14 @@ router.get('/', (req, res) => {
         'created_at',
       ],
       include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
